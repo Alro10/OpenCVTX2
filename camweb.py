c2a033277a498a3d7072becd8e367eb6fdbf5676
Alexander Robles <alexander.robles20@gmail.com>
2018-05-16 16:10:53 -0300

    onboard cam and webcam

---
+++ camweb.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("/dev/video1")
+
+while(True):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+
+  # Our operations on the frame come here
+  grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  grey_3_channel = cv2.cvtColor(grey, cv2.COLOR_GRAY2BGR)
+
+  numpy_horizontal = np.hstack((frame, grey_3_channel))
+  numpy_horizontal_concat = np.concatenate((frame, grey_3_channel), axis=1)	
+
+  # Display the resulting frame
+  cv2.imshow('Numpy Horizontal Concat', numpy_horizontal_concat)
+
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
